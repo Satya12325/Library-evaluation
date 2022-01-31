763bdf6da27b16cc03ae5b8df08e6820abd1547d
Satyabrata Das <satyabrata_pw1_135@masai.school>
2022-01-31 21:52:23 +0530

    squire area perimeter

--- Areaa.js
+++ Areaa.js
@@ -2,9 +2,16 @@ class Shape2D {
     constructor(side1, side2) {
       this.area = side1 * side2;
       this.perimeter = 2*(side1 + side2)
+      this.getPerimeterSquire = 4*side1
     }
   }
 
+  class Square extends Shape2D {
+    constructor(side) {
+      super(side, side);
+    }
+  }
+  
  
 
 class Rectangle extends Shape2D {
@@ -13,3 +20,4 @@ class Rectangle extends Shape2D {
     }
   }
   console.log(new Rectangle(6,12).area,new Rectangle(6,12).perimeter)
+  console.log(new Square(5).area,new Square(5).getPerimeterSquire)
